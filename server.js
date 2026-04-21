272976b1ef12be2b635eed9f116d58084adbca3b
Angshuman <asarkarwork2002@gmail.com>
2026-04-21 23:13:36 +0530

    demo changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 let tasks = [];
 
 app.get('/', (req, res) => {
-    res.send('CI/CD AUTOMATION SUCCESSFULY UPDATED');
+    res.send('CI/CD Working LIve Demo');
 });
 
 app.get('/health', (req, res) => {
